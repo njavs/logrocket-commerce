fa44321a3e9be18b6fd8ed518223b1a61ef39aa3
Your Name <you@example.com>
2020-11-22 18:54:06 -0800

    Change logo

--- components/ui/Logo/Logo.tsx
+++ components/ui/Logo/Logo.tsx
@@ -1,4 +1,4 @@
-const Logo = ({ className = '', ...props }) => <img src={"https://icon-library.com/images/icon-clothing/icon-clothing-4.jpg"} alt="Logo" />
+const Logo = ({ className = '', ...props }) => <img src={"https://icon-library.com/images/icon-clothing/icon-clothing-4.jpg"} width="40" height="40" alt="Logo" />
 
 export default Logo
 
