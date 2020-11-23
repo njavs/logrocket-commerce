6d0aed0d39835237637acb3c6f9b30d8625892a3
Your Name <you@example.com>
2020-11-22 18:32:02 -0800

    Change logo

--- components/ui/Logo/Logo.tsx
+++ components/ui/Logo/Logo.tsx
@@ -1,6 +1,8 @@
+
 const Logo = ({ className = '', ...props }) => (
+  
 <svg width="90" height="90">       
-     <image xlink:href="https://images.vexels.com/media/users/3/142750/isolated/preview/ff94b207d7c284811f2c939f46aeb16b-flat-blue-blazer-clothing-icon-by-vexels.png" src="https://images.vexels.com/media/users/3/142750/isolated/preview/ff94b207d7c284811f2c939f46aeb16b-flat-blue-blazer-clothing-icon-by-vexels.png" width="90" height="90"/>    
+     <image href="https://images.vexels.com/media/users/3/142750/isolated/preview/ff94b207d7c284811f2c939f46aeb16b-flat-blue-blazer-clothing-icon-by-vexels.png" src="https://images.vexels.com/media/users/3/142750/isolated/preview/ff94b207d7c284811f2c939f46aeb16b-flat-blue-blazer-clothing-icon-by-vexels.png" width="90" height="90"/>    
 </svg>
 )
 
